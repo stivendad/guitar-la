84cabef2668f5840e41e4d1cf00d3d0efb735e72
Daniel Tejada <dastte93@gmail.com>
2024-05-11 10:14:07 -0500

    Inicio del proyecto con state de carrito y db

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+import { Guitar } from './components/Guitar'
+import { db } from './data/db'
+
+function App() {
+
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExist >= 0) {
+
+      console.log('Ya existe...');
+      const updaetdCart = [...cart];
+      updaetdCart[itemExist].quantity++;
+      setCart(updaetdCart);
+
+    } else {
+
+      item.quantity = 1;
+      setCart([...cart, item]);
+
+    }
+
+
+  }
+
+  return (
+
+    <>
+      <Header />
+
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
+
+
+        </div>
+      </main>
+
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
